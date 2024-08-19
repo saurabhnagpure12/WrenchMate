7015cd368366f5b56d69de8eaf02e262e3884163
Saurabhn16 <nagpuresaurabh1610@gmail.com>
2024-08-19 11:43:29 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Navbar from "./pages/Navbar";
 import TermsOfService from "./pages/term";
-import PrivacyPolicy from "./pages/privacy";
+import PrivacyPolicy from "./pages/Privacy";
 
 const App = () => {
-
-  return (<>
-<Navbar/>
-    <Routes>   
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<LandingPage />} />
-      <Route path="/term" element={<TermsOfService/>} />
-       <Route path="/privacy" element={<PrivacyPolicy />} />
-    </Routes>
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<LandingPage />} />
+        <Route path="/term" element={<TermsOfService />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+      </Routes>
     </>
   );
 };
